fa1bf74d7db9e6fd56f87fd36a0dfc3a9826db25
Enricco Gemha <riccogemha@gmail.com>
2023-03-08 19:00:12 -0300

    finalizado aleluia

--- atv/03-funcoes/solucao.c
+++ atv/03-funcoes/solucao.c
@@ -1,1 +1,10 @@
 // TODO implemente aqui uma funcao chamada "solucao"
+
+int solucao(int edi, long rsi, long *rdx) {
+    int eax = rsi * 5;
+    eax = eax + edi;
+    *rdx = eax;
+    eax = rsi * 3;
+    eax = eax + (4 * edi);
+    return eax;
+}
