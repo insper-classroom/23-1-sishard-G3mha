511f1c7fad59917b68626aedaf270bdade34712f
Enricco Gemha <riccogemha@gmail.com>
2023-03-09 15:35:10 -0300

    solucao ex2

--- atv/04-condicionais-funcoes/solucao_ex2.c
+++ atv/04-condicionais-funcoes/solucao_ex2.c
@@ -1,3 +1,12 @@
 long vezes2(long a);
 
 /* Implementar função solucao_ex2 */
+
+// rdi = a, rsi = b, rbx = c
+long solucao_ex2(long a, long b){
+    long c = a;
+    long retval = vezes2(b);
+    c = (retval > c) ? c + c : c;
+    retval += c;
+    return retval;
+}
