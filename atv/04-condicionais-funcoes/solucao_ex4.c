4c383319c69be7f35b016462e9493d0a72480bbb
Enricco Gemha <riccogemha@gmail.com>
2023-03-12 18:58:17 -0300

    ex4 finalizado

--- atv/04-condicionais-funcoes/solucao_ex4.c
+++ atv/04-condicionais-funcoes/solucao_ex4.c
@@ -1,1 +1,12 @@
 /* Implementar função solucao_ex4 */
+
+int solucao_ex4(short di, int esi) {
+    if (di <= 0x11)
+        return 0x12 - di;
+    
+    if ((unsigned char) esi - 0x41 > 1)
+        return 0xffffffff;
+    
+    di -= 0x11;
+    return di;
+}
