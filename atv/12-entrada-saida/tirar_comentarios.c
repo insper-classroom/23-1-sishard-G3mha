a8932ef24706d2c797fa807b217c8b537058b538
Enricco Gemha <riccogemha@gmail.com>
2023-04-23 22:09:20 -0300

    parece que foi!

--- atv/12-entrada-saida/tirar_comentarios.c
+++ atv/12-entrada-saida/tirar_comentarios.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) { 
+int main (int argc, char *argv[]) { 
+    
+    int fd_src = open(argv[1], O_RDONLY);
+    int fd_resp = open(argv[2], O_WRONLY);
 
+
+
+    return 0;
 }
