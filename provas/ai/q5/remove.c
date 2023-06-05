c458ca433dcca43cd417d19ffa85bafcd81d487b
Enricco Gemha <riccogemha@gmail.com>
2023-06-05 13:50:05 -0300

    vamo

---
+++ provas/ai/q5/remove.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Seu código neste arquivo!
+char *remove_comentarios(char * funcao){
+    // EDITE ESTA FUNÇÃO, INCLUSIVE O RETURN ABAIXO!
+    return NULL;    
+}
+
